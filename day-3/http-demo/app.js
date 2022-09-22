3730d6d9476d79cbd211f71ad49f1a7eea8f7001
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-09-22 21:59:02 +0530

    http : node http module

--- day-3/http-demo/app.js
+++ day-3/http-demo/app.js
@@ -16,6 +16,7 @@ const onRequest = (req, res) => {
         })
         req.on('end', () => {
             console.log(body)
+            res.writeHead(201)
             res.end('success')
         })
     }else {
