02d56ea1dc82b5ad8d7e0b6368e3032f7896e668
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-10-03 20:32:22 +0530

    middleware

--- day-7-8/crud-app/app.js
+++ day-7-8/crud-app/app.js
@@ -5,12 +5,34 @@ mongoose.connect('mongodb://localhost:27017/test').then( () => {
     console.log('❌ There is a problem in connection')
 })
 
-const Car = mongoose.model('Car', {
-    model : String,
-    country : Number,
-    isFwd : Boolean,
-    make : String
-}) 
+const CarSchema = new mongoose.Schema({
+    model : { type : String, unique : true },
+    country : { type : Number},
+    isFwd : { type : Boolean },
+    make : { type : String }
+})
+
+CarSchema.pre('save', next => {
+    console.log('PRE : Middleware Executed')
+    next()
+})
+
+CarSchema.post('save', doc => {
+    console.log('POST : Middleware Executed')
+    console.log('ID : ' + doc._id)
+})
+
+CarSchema.post('save', (error, doc, next) => {
+    console.log('POST : Middleware Executed')
+    console.log('ID : ' + doc._id)
+    if(error.name === 'MongoServerError' && error.code === 11000) {
+        next(new Error('Model Should be Unique'))
+    } else {
+        next()
+    }
+})
+
+const Car = mongoose.model('Car', CarSchema )
 
 const express = require('express')
 const app = express()
@@ -20,20 +42,15 @@ app.post('/', express.json() ,(req, res) => {
     const reqCar = req.body // fetch the data from request
     console.log(reqCar)
 
-    // const dbObj = new Car({ 
-    //     model : reqCar.model, 
-    //     make : reqCar.make,
-    //     isFwd : reqCar.isFwd,
-    //     country : reqCar.country
-    // })
-
     //es6 version
     const dbObj = new Car({ ...reqCar })
 
     dbObj.save().then(
         sts => res.status(201).json({ sts : 'success' })
     ).catch(
-        err => res.status(400).json({ sts : 'err' })
+        err => {
+            res.status(400).json({ sts : 'err', msg : err.message })
+        }
     )
 })
 
